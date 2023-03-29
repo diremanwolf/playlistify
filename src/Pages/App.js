62a2a89318aebefc1a06d953660e5d86b6363109
diremanwolf <dantesamaelfernandezlago@gmail.com>
2023-03-29 13:55:54 -0500

    Modify: App component.

---
+++ src/Pages/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1><span className="highlight">Playlist</span>-ify</h1>
+      <div className="App">
+        <div className="App-playlist">
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+// SearchBar: 8
+// SearchResults: 10
+// Playlist: 11
