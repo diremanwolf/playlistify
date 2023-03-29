f78f442e81c8268a02266ced7b63cfc5144132eb
diremanwolf <dantesamaelfernandezlago@gmail.com>
2023-03-29 17:26:49 -0500

    Create: change playlist name method.

--- src/Components/Playlist/Playlist.component.js
+++ src/Components/Playlist/Playlist.component.js
@@ -4,10 +4,21 @@ import React from 'react';
 import TrackList from '../TrackList/TrackList.component';
 
 class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.props.onNameChange(event.target.value);
+  }
+
+
   render() {
     return (
       <div className="Playlist">
-        <input defaultValue={this.props.name} />
+        <input defaultValue={this.props.name} onChange={this.handleNameChange}/>
         <TrackList tracks={this.props.tracks} isRemoval={true} onRemove={this.props.onRemove} />
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
--- src/Pages/App.js
+++ src/Pages/App.js
@@ -27,6 +27,7 @@ class App extends React.Component {
 
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -43,6 +44,10 @@ class App extends React.Component {
     this.setState({ playlistTracks: tracks });
   }
 
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
+  }
+
   render() {
     return (
       <div>
@@ -51,7 +56,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <Playlist name={this.state.playlistName} tracks={this.state.playlistTracks} onRemove={this.removeTrack} />
+            <Playlist name={this.state.playlistName} tracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} />
           </div>
         </div>
       </div>
