e2dd12a45d9026ed4ee1a14a8720447af265e612
diremanwolf <dantesamaelfernandezlago@gmail.com>
2023-03-29 13:59:48 -0500

    Create: SearchBar component.

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 
+import SearchBar from './Components/SearchBar/SearchBar.component';
+
 function App() {
   return (
     <div>
       <h1><span className="highlight">Playlist</span>-ify</h1>
       <div className="App">
+        <SearchBar />
         <div className="App-playlist">
         </div>
       </div>
