0e1b5c45767065463c35495344ea43e09871197a
diremanwolf <dantesamaelfernandezlago@gmail.com>
2023-03-29 14:27:09 -0500

    Modify: plus/minus action based on flag.

--- src/Pages/Components/Track/Track.component.js
+++ src/Pages/Components/Track/Track.component.js
@@ -3,6 +3,10 @@ import './Track.component.css';
 import React from 'react';
 
 class Track extends React.Component {
+  renderAction() {
+    return (this.props.remove) ? <p className='Track-action'>-</p> : <p className='Track-action'>+</p>;
+  }
+
   render() {
     return (
       <div className="Track">
@@ -10,7 +14,7 @@ class Track extends React.Component {
           <h3>{this.props.name}</h3>
           <p>{this.props.artist} | {this.props.album}</p>
         </div>
-        <button className="Track-action">{ /*<!-- + or - will go here -->*/ }</button>
+        <button className="Track-action">{this.renderAction()}</button>
       </div>
     );
   }
--- src/Pages/Components/TrackList/TrackList.component.js
+++ src/Pages/Components/TrackList/TrackList.component.js
@@ -2,14 +2,13 @@ import './TrackList.component.css';
 
 import React from 'react';
 import Track from '../Track/Track.component';
-
 class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        <Track name='Blanco White' artist='Nocturne' album='Nocturne EP' />
-        <Track name='Borderline' artist='Tame Impala' album='The Slow Rush' />
-        <Track name='Save Your Tears' artist='The Weekend' album='After Hours' />
+        <Track name='Blanco White' artist='Nocturne' album='Nocturne EP' remove={true} />
+        <Track name='Borderline' artist='Tame Impala' album='The Slow Rush' remove={false} />
+        <Track name='Save Your Tears' artist='The Weekend' album='After Hours' remove={true} />
       </div>
     );
   }
